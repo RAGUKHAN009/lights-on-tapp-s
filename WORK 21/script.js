6671a77c4889135f2d09b41f4e49197c899c5113
Aliyan Arif <143800651+RAGUKHAN009@users.noreply.github.com>
2023-11-24 22:51:24 +0500

    Add files via upload

---
+++ WORK 21/script.js
@@ -0,0 +1,8 @@
+function apear() {
+  var div = document.getElementById('hidde');
+  if (div.style.display === 'none') {
+    div.style.display = 'block';
+  } else {
+    div.style.display = 'none';
+  }
+}
